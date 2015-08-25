c5e8049740724f32b70af6f0d7fd669b4cc2820c
m <michal@mdevel.net>
2015-08-25 12:11:23 +0200

    fixed docker startup script

--- docker/start-nosql.sh
+++ docker/start-nosql.sh
@@ -11,16 +11,24 @@ fi
 
 CACHE_DIR=${CACHE_DIR:-"/tmp/tsdb-nosql"}
 TSDB_HOME=${TSDB_HOME:-.}
-TSDB_PORT=${TSDB_PORT:-4242}
+BINDPORT=${BINDPORT:-4242}
 export JAVA=${JAVA:-"java"}
 export JVMARGS=${JVMARGS:-"-enableassertions -enablesystemassertions -Xmx3000m"}
+
+if [ ! -z "$HOSTS_ENTRY" ]; then
+    grep "$HOSTS_ENTRY" /etc/hosts
+    if [ $? == 1 ]; then
+        # this is needed to resolve dns name reported by hbase
+        echo "$HOSTS_ENTRY" >> /etc/hosts
+    fi
+fi
 
 mkdir -p $CACHE_DIR
 mkdir -p $LOG_DIR
 rm -rf $CACHE_DIR/*
 
 exec $TSDB_HOME/build/tsdb tsd \
- --port=$TSDB_PORT \
+ --port=$BINDPORT \
  --staticroot=$TSDB_HOME/build/staticroot \
  --cachedir="$CACHE_DIR" \
  --zkquorum $ZKQUORUM \
